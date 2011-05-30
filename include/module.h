6aa7d590ef2f4e9cba2c106aef9ac2705855fa32
Jan Tusil <jenda.tusil@gmail.com>
2011-05-30 21:02:33 +0200

    Module support. I think the interface should not change in near future.

--- include/module.h
+++ include/module.h
@@ -1,14 +1,82 @@
 #ifndef MODULE_H
 #define MODULE_H
 
-#include <arch/arch.h>
-int modules_init(void);
+#define MODULE_CNT 8
+
+#include <emux51.h>
+
 
 typedef struct {
-	unsigned  int id;
-	unsigned long int bits;
-	struct sockaddr_in addr;
+/*	handle for dynamic linker	*/
+	void *handle;
+/*	now only idx to array		*/
+	int id;
+} modid_t;
+
+
+/*
+ *	Functions, provided by emulator for module.
+ */
+
+typedef struct {
+	char (*read_port)   (modid_t id, int port);
+	int  (*write_port)  (modid_t id, int port, char data);
+	int  (*alloc_bits)  (modid_t id, int port, char mask);
+	int  (*free_bits)   (modid_t id, int port, char mask);
+	int  (*handle_event)(modid_t id, const char *event, void (*handle)(int));
+	int  (*queue_add)   (modid_t id, unsigned cycles, void (*event)(void));
+
+} emuf_t;
+
+/*
+ *	Functions, provided by the module itself.
+ *
+ *	See that 'read' function has type 'void'.
+ *	These calls are only notifications for module.
+ *	Module itself perform own operation.
+ *
+ *	Note that if emulator call modf_t::read,
+ *	module will perform 'write' operation.
+ */
+
+typedef struct {
+	/*	only these functions are necessary	*/
+	void (*init) (modid_t id, emuf_t *callbacks);
+	void (*exit) (const char *reason);	
+	void (*write)(int port);
+	void (*read) (int port);
+
+
+} modf_t;
+
+
+
+typedef struct {
+	modid_t id;
+	char mask[PORTS_CNT];
+	emuf_t callbacks;
+	modf_t f;
+
 } module_t;
 
+/*	delta array type	*/
+typedef struct DLIST {
+	void (*f)(void);
+	unsigned dt;
+	struct DLIST *next;
+	
+} dlist_t;
+
+
+int module_new(char *path);
+
+void module_import_port(int port);
+void module_export_port(int port);
+
+void module_queue_perform(int steps);
+int module_queue_add(modid_t modid, unsigned cycles, void (*f)(void));
+
+
+int modules_init(void);
 
 #endif
--- src/module.c
+++ src/module.c
@@ -5,185 +5,306 @@
 #include <pthread.h>
 #include <signal.h>
 #include <ctype.h>
+#include <dlfcn.h>
 
 #include <emux51.h>
 #include <module.h>
 #include <arch/arch.h>
 
-#define MODULES_MAX 32
-
-module_t modules[MODULES_MAX];
-unsigned int module_cnt;
-
-pthread_t module_thread_id;
-
-unsigned long used_bits;
 
 
-/*	protocol:
- *	0xid	action	0xdata
- *	actions:register, read, write, unregister
- */
+dlist_t *dlist_first=NULL;
+dlist_t *dlist_last=NULL;
+
+char port_usage[PORTS_CNT];
+module_t modules[MODULE_CNT];
 
 
 
-/*	TODO:	*/
-
-int module_register(unsigned int req_bits, struct sockaddr_in addr)
+inline int test_module_id(modid_t modid)
 {
-	unsigned long int id;
-	int i;
-	int empty;
-
-	printf("registering mask 0x%x\n", req_bits);
-	printf("port == %u\n", ntohs(addr.sin_port));
-
-	if (module_cnt == MODULES_MAX)
+	if (modid.id >= MODULE_CNT)
 		return -1;
-	if (used_bits&req_bits)
+	if (modules[modid.id].id.id != modid.id)
+		return -1;
+	if (modules[modid.id].id.handle !=modid.handle)
+		return -1;
+	return 0;
+}
+
+
+
+int module_load(char *path, module_t *mod)
+{
+	mod->id.handle=dlopen(path, RTLD_LAZY);
+	if (mod->id.handle == NULL) {
+		fprintf(stderr, "cannot load module from %s:\n"
+				"%s\n", path, dlerror());
+		return (-1);
+	}
+	mod->f.init=dlsym (mod->id.handle,	"module_init" );
+	mod->f.exit=dlsym (mod->id.handle,	"module_exit" );
+/*		TODO: REMOVE:		*/
+/*
+	mod->f.write=dlsym(mod->id.handle,	"module_read" );
+	mod->f.read=dlsym (mod->id.handle,	"module_write");
+*/
+
+	if (!(mod->f.init && mod->f.exit)){
+		fprintf(stderr, "%s is bad module\n", path);
+		dlclose(mod->id.handle);
 		return -2;
-
-	for (i=0; i<MODULES_MAX; i++) {
-		if (modules[i].id == 0)
-			break;
 	}
-	if (i >= MODULES_MAX) {
-		/*	this would never happen	*/
-		fprintf(stderr, "%s: %d: wtf?\n", __FILE__, __LINE__);
-		return -3;
-	}
-	empty=i;
 
-	do {
-		id=1+rand();
-		for(i=0; i<MODULES_MAX; i++){
-			if (modules[i].id == id){
-				id=0;
-				break;
-			}
-		}
-	}while(!id);
-
-	module_cnt++;
-	modules[empty].id=id;
-	modules[empty].bits=req_bits;
-	modules[empty].addr=addr;
-
-	used_bits|=req_bits;
-	
+	memset(mod->mask, 0, PORTS_CNT);
 
 	return 0;
 }
 
-int module_unregister(unsigned int id)
+/*******************************************************************************
+ *		<API for modules>
+ *
+ *	API for modules is based on this functions:
+ *	*	module_alloc_bits,
+ *	*	module_free_bits,
+ *	*	module_read_port
+ *	*	module_write_port.
+ *
+ ******************************************************************************/
+
+
+
+
+int module_alloc_bits(modid_t modid, int port, char mask)
 {
-	int i;
-	for (i=0; i<module_cnt; i++){
-		if(modules[i].id == id) {
-			used_bits&=~modules[i].bits;
-			memset(&modules[i], 0, sizeof(module_t));
+	/*	somebody is cheating	*/
+	if (port >= PORTS_CNT || test_module_id(modid) || exporting) {
+		fprintf(stderr, "somebody is cheating\n");
+		return -1;
+	}
+	/*	any bits are alloced	*/
+	if (port_usage[port]&mask){
+		return (port_usage[port]&mask);
+	}
+	port_usage[port]|=mask;
+	modules[modid.id].mask[port]|=mask;
+	return 0;
+}
+
+int module_free_bits(modid_t modid, int port, char mask)
+{
+	if (port >= PORTS_CNT || test_module_id(modid) || exporting) {
+		fprintf(stderr, "somebody is cheating\n");
+		return -1;
+	}
+	mask&=modules[modid.id].mask[port];
+	port_usage[port]&=~mask;
+	modules[modid.id].mask[port]&=~mask;
+	return 0;
+}
+
+
+int module_write_port(modid_t modid, int port, char data)
+{
+	char old;
+	char new;
+	
+	if (port >= PORTS_CNT || test_module_id(modid) || exporting) {
+		fprintf(stderr, "somebody is cheating\n");
+		return -1;
+	}
+	printf("module_write_port %d\n", port);
+	old=read_port(port);
+	new=old;
+	new|=data& modules[modid.id].mask[port];
+	new&=data|~modules[modid.id].mask[port];
+	write_port(port, new);
+	return 0;
+}
+
+char module_read_port(modid_t modid, int port)
+{
+	if (port >= PORTS_CNT || test_module_id(modid)) {
+		fprintf(stderr, "somebody is cheating\n");
+		return -1;
+	}
+	printf("module_read_port %d\n", port);
+	return(read_port(port));	
+}
+
+/*	I hope this is all right ;-)	*/
+int module_queue_add(modid_t modid, unsigned cycles, void (*f)(void))
+{
+	dlist_t *entry;
+	dlist_t *new;
+
+	if (test_module_id(modid)) {
+		fprintf(stderr, "somebody is cheating\n");
+		return -1;
+	}
+	new=malloc(sizeof(dlist_t));
+	if (new == NULL) {
+		fprintf(stderr, "OOM while queueing\n");
+		return -2;
+	}
+	new->f=f;
+
+	entry=dlist_first;
+	while (entry) {
+		if (entry->dt <= cycles) {
+			cycles-=entry->dt;
+			entry=entry->next;
+		}
+		else {
+			new->next=entry->next;
+			entry->next=new;
+			new->dt=cycles;
+			if (new->next){
+				new->next->dt-=new->dt;
+			}
+			else {
+				dlist_last=new;
+			}
 			return 0;
 		}
 	}
-	fprintf(stderr, "cannot unregister module '%x'\n", modules[i].id);
-	return -1;
-}
-void *module_thread(void *parameter)
-{
-	int s;
-	size_t b;
-	socklen_t len;
-	struct sockaddr_in host, me;
-
-	char buff[80];
-	char cmd[20];
-	unsigned int id;
-	char action;
-	unsigned int data;
-/*
-	sscanf("12 ble 36", "%x %s %x", &id, command, &data);
-	printf("command: %s\n", command);
-*/
-	printf("sizeof(buff)== %d\n", sizeof(buff));
-
-	signal(SIGALRM, SIG_IGN);
-	len=sizeof(host);
-	memset(&host, 0, len);
-	memset(&me, 0, len);
-
-	s=socket(AF_INET, SOCK_DGRAM, 0);
-	if (s < 0) {
-		perror("socket");
-		return NULL;
-	}
-	me.sin_family=AF_INET;
-	me.sin_addr.s_addr=INADDR_ANY;
-	me.sin_port=htons(8000);
-
-	if (0 > bind(s, (struct sockaddr *)&me, sizeof(struct sockaddr))){
-		perror("bind");
-		return NULL;
-	}
-
-	while (1) {
-		b=recvfrom(s, &buff, sizeof(buff), 0,
-			(struct sockaddr *)&host, &len);
-		if (b == -1) {
-			perror("recvfrom");
-			return NULL;
-		}
-		/*	in connection-less protocol, this cannot occur	*/
-		if (b == 0) {
-			fprintf(stderr, "I though this cannot occcur\n");
-			continue;
-		}
-		if (sscanf(buff, "%x %s %x", &id, cmd, &data) != 3) {
-			fprintf(stderr, "invalid message\n");
-			continue;
-		}
-
-		if (id == 0) {
-			if (!strcmp(cmd, "register")){
-				module_register(data, host);
-			}
-			else if (!strcmp(cmd, "read")){
-				
-			}
-		}
-
-
-		
-	};
-
-
-
-	
-	return NULL;
-}
-
-
-
-int modules_init(void)
-{
-	int rval;
-#ifdef WIN32
-	WSADATA data;
-	if (WSAStartup(MAKEWORD(1, 1), &data)) {
-		fprintf(stderr, "WSAStartup\n");
-		return -1;
-	}
-#endif
-/*	broadcast_send();*/
-	srand(time(NULL));
-	memset(modules, 0, MODULES_MAX*sizeof(module_t));
-	rval=pthread_create(&module_thread_id, NULL, module_thread, NULL);
-	if (rval){
-		perror("pthread_create");
-		return -1;
-	}
-	while (1)
-		getchar();
-	
+	dlist_first=dlist_last=new;
+	new->next=NULL;
+	new->dt=cycles;
 
 	return 0;
 }
+
+void module_queue_perform(int steps)
+{
+	dlist_t *entry;
+	dlist_t *tmp;
+
+	entry=dlist_first;
+	int i=0;
+/*	printf("steps == %d\n", steps);*/
+	while (entry) {
+		i++;
+		if (entry->dt < steps) {
+			/*	perform the operation	*/
+			/*printf("going to perform operation..\n");*/
+			if (entry->f)
+				entry->f();
+			else printf("empty f?, i == %d\n", i);
+			steps-=entry->dt;
+
+			/*	free current node	*/
+			tmp=entry->next;
+			free(entry);
+			entry=tmp;
+			dlist_first=entry;
+		}
+		else {
+/*			printf("dt-=%d\n", steps);*/
+			entry->dt-=steps;
+			return;
+		}
+	}
+	/*	nothing to do	*/
+	return;
+}
+
+int handle_event(modid_t modid, const char *event, void (*handle)(int))
+{
+	module_t *mod;
+
+	if (test_module_id(modid)) {
+		fprintf(stderr, "somebody is cheating\n");
+		return -1;
+	}
+	mod=&modules[modid.id];
+
+	if (!strcmp(event, "read"))
+		mod->f.write=handle;	/*	;)	*/
+	else
+	if (!strcmp(event, "write"))
+		mod->f.read=handle;
+	else
+		return -1;
+	return 0;
+}
+
+/*******************************************************************************
+ *		</API for modules>
+ ******************************************************************************/
+ 
+
+
+/*			<emulator API>				*/
+
+void module_import_port(int port)
+{
+	int i;
+	module_t *mod;
+	printf("module_import_port\n");
+
+	for (i=0; i<MODULE_CNT; i++) {
+		mod=&modules[i];
+		if(mod->mask[port] && mod->f.read)
+			mod->f.read(port);
+	}
+}
+
+void module_export_port(int port)
+{
+	int i;
+	module_t *mod;
+
+	for(i=0; i<MODULE_CNT; i++) {
+		mod=&modules[i];
+		if (mod->id.handle && mod->f.write){
+			mod->f.write(port);
+		}
+	}
+}
+
+int module_new(char *path)
+{
+	int i;
+	module_t *mod;
+
+	if (path == NULL) {
+		return -1;
+	}
+
+
+	for(i=0; i<MODULE_CNT; i++) {
+		if (modules[i].id.handle == NULL) {
+			mod=&modules[i];
+			break;
+		}
+	}
+	if (i == MODULE_CNT) {
+		fprintf(stderr, "module_new failed: there's no space 4U\n");
+		return -1;
+	}
+	if (module_load(path, mod)){
+		return -2;
+	}
+
+	mod->id.id=i;
+
+	mod->callbacks.alloc_bits=module_alloc_bits;
+	mod->callbacks.free_bits=module_free_bits;
+	mod->callbacks.read_port=module_read_port;
+	mod->callbacks.write_port=module_write_port;
+	mod->callbacks.handle_event=handle_event;
+	mod->callbacks.queue_add=module_queue_add;
+
+	mod->f.init(mod->id, &mod->callbacks);
+	return 0;
+}
+
+int modules_init(void)
+{
+	memset(modules, 0, MODULE_CNT*sizeof(module_t));
+	return 0;
+
+}
+
+/*			</emulator API>				*/
